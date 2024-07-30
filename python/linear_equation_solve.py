15ee60b8fc07101a6112269ae5863916ff10144c
ArzygulIm <arzygul.imankulova.kk@gmail.com>
2024-07-30 22:36:58 +0600

    30.07.24

--- python/linear_equation_solve.py
+++ python/linear_equation_solve.py
@@ -4,10 +4,13 @@ def solve_linear_equation_with_one_l(equation: str) -> dict:
     :return:
     """
     if (len(equation.split("=")) == 2 and len(equation.split("=")[0]) != 0 and len(equation.split("=")[1]) != 0):
-        print("if work")
+        # print("if work")
         left_side = equation.replace(" ", "").split("=")[0].replace("-", "+-").split("+")
         right_side = equation.replace(" ", "").split("=")[1].replace("-", "+-").split("+")
-
+        return{
+            "solution":[],
+            "answer":""
+        }
     else:
         return ({
             "kg": "Тендеме туура эмес жазылган!",
